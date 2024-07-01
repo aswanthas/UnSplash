a9cd2c3c01b5894425febf1e223a469d9593fd16
Aswa_nth* <aswanthas124@gmail.com>
2024-07-01 17:59:07 +0530

    Initial Commit

---
+++ UnSplash/UnSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnSplashApp.swift
+//  UnSplash
+//
+//  Created by Aswanth K on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct UnSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
