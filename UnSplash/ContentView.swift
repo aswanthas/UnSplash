ee2eaa3611156a3c04942313307b1acfecdc6a12
Aswa_nth* <aswanthas124@gmail.com>
2024-07-02 22:13:25 +0530

    add splash screen

--- UnSplash/ContentView.swift
+++ UnSplash/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAppActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Image("splashImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Text("Image")
+                    .font(Font.custom("Baskerville-Bold", size: 20))
+                    .foregroundStyle(.black.opacity(0.5))
+            }
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear(perform: {
+                withAnimation(.easeInOut(duration: 1.2)) {
+                    self.size = 0.9
+                    self.opacity = 1.0
+                }
+            })
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.isAppActive = true
+            }
+        }
     }
 }
 
